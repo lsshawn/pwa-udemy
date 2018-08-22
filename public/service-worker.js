6657d56ea9aa1a9fd2818c298baa0b9ef5ee0d4f
Shawn <coffee.or.tea88@gmail.com>
2018-08-23 00:45:04 +0700

    workbox with routes

---
+++ public/service-worker.js
@@ -0,0 +1,120 @@
+importScripts('workbox-sw.prod.v2.1.3.js');
+
+const workboxSW = new self.WorkboxSW()
+
+workboxSW.router.registerRoute(
+  /.*(?:googleapis|gstatic)\.com.*$/,
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts',
+    cacheExpiration: {
+      maxEntries: 3,
+      maxAgeSeconds: 60 * 60 * 24 * 30 // update every month
+    }
+  })
+)
+
+workboxSW.router.registerRoute(
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'material-css'
+  })
+)
+
+workboxSW.router.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'post-images'
+  })
+)
+
+
+workboxSW.precache([
+  {
+    "url": "favicon.ico",
+    "revision": "2cab47d9e04d664d93c8d91aec59e812"
+  },
+  {
+    "url": "index.html",
+    "revision": "481c0fd6ac642a35d31832b9714c4bc4"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "d11c7965f5cfba711c8e74afa6c703d7"
+  },
+  {
+    "url": "offline.html",
+    "revision": "635d7ca28c2520423fd8d335609718ca"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "0d20716c4867b76e7147cad96eaa1162"
+  },
+  {
+    "url": "src/css/app.css",
+    "revision": "f27b4d5a6a99f7b6ed6d06f6583b73fa"
+  },
+  {
+    "url": "src/css/feed.css",
+    "revision": "14ecc3d16fed80a13f140b4d5873877b"
+  },
+  {
+    "url": "src/css/help.css",
+    "revision": "1c6d81b27c9d423bece9869b07a7bd73"
+  },
+  {
+    "url": "src/js/app.js",
+    "revision": "1644f95ddcb9c6dad6c056289922ee8f"
+  },
+  {
+    "url": "src/js/feed.js",
+    "revision": "fd74a651ce3323c5e13a7c2a02a7c5e3"
+  },
+  {
+    "url": "src/js/fetch.js",
+    "revision": "6b82fbb55ae19be4935964ae8c338e92"
+  },
+  {
+    "url": "src/js/idb.js",
+    "revision": "017ced36d82bea1e08b08393361e354d"
+  },
+  {
+    "url": "src/js/material.min.js",
+    "revision": "713af0c6ce93dbbce2f00bf0a98d0541"
+  },
+  {
+    "url": "src/js/promise.js",
+    "revision": "10c2238dcd105eb23f703ee53067417f"
+  },
+  {
+    "url": "src/js/utility.js",
+    "revision": "9778d39e7fa97be9cb228dc89e7350f6"
+  },
+  {
+    "url": "sw-base.js",
+    "revision": "2173c9198531efff9e0d79bf7900df79"
+  },
+  {
+    "url": "sw.js",
+    "revision": "6423066445f842757ca1fb2fdf3ead95"
+  },
+  {
+    "url": "workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  },
+  {
+    "url": "src/images/main-image-lg.jpg",
+    "revision": "31b19bffae4ea13ca0f2178ddb639403"
+  },
+  {
+    "url": "src/images/main-image-sm.jpg",
+    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
+  },
+  {
+    "url": "src/images/main-image.jpg",
+    "revision": "5c66d091b0dc200e8e89e56c589821fb"
+  },
+  {
+    "url": "src/images/sf-boat.jpg",
+    "revision": "0f282d64b0fb306daf12050e812d6a19"
+  }
+])
